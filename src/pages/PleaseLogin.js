f0b5e0d6aee8f862918f8eb93376417bb111aae5
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-07 21:56:51 +0300

    Add files via upload

--- src/pages/PleaseLogin.js
+++ src/pages/PleaseLogin.js
@@ -7,18 +7,13 @@ function PleaseLogin () {
 
     return (
         <div className="scroll-smooth bg-blue-600 w-full h-screen flex flex-col justify-center items-center">
-            <button onClick={reload}>
+            <a href="/">
                 <EmojiProvider data={emojiData} ><Emoji name="face-with-rolling-eyes" width={120} /></EmojiProvider>
-            </button>
-            <button onClick={reload} className="mt-10 md:mt-16">
-                <h2 className="text-xl md:text-5xl text-white hover:text-gray-300 transition-colors duration-500 font-black">You need to be logged in to view this page...</h2>
-            </button>
+            </a>
+            <a href="/" className="mt-10 md:mt-16 text-xl md:text-5xl text-white hover:text-gray-300 transition-colors duration-500 font-black">You need to be logged in to view this page...</a>
         </div>
     );
 }
 
-function reload() {
-    window.location.href = '/';
-}
 
 export default PleaseLogin;
