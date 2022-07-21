9c09804bd05c3ddc48681fcb1490aa56fbe14525
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-21 17:39:40 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ function Home () {
     return (
         <div className="scroll-smooth bg-black">
             <Header />
-            <div id="first_div" className="w-full h-screen bg-red-600 transition-colors duration-700">
+            <div id="first_div" className="w-full h-screen bg-blue-600 transition-colors duration-700">
                 <div className="flex flex-col justify-center max-w-xs mx-auto w-full h-5/6">
                     <button id="sign_in_btn" className='max-w-xs mx-auto bg-white shadow-2xl rounded-full w-48 md:w-64 h-10 md:h-12 text-sm md:text-base items-center text-blue-500 font-bold select-none hover:scale-110 ease-in-out duration-300'>
                         Connect
