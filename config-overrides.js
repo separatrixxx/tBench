86983b95e96ffa4f206cfcaebd9d16e5505c19ec
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-23 11:54:59 +0300

    Add files via upload

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,22 @@
-const webpack = require("webpack");
+const webpack = require("webpack")
 
-module.exports = function override(config) {
-    const fallback = config.resolve.fallback || {};
-    Object.assign(fallback, {
-        crypto: require.resolve("crypto-browserify"),
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
         stream: require.resolve("stream-browserify"),
-        assert: require.resolve("assert"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        os: require.resolve("os-browserify"),
-        url: require.resolve("url"),
-    });
-    config.resolve.fallback = fallback;
-    config.ignoreWarnings = [/Failed to parse source map/];
-    config.plugins = (config.plugins || []).concat([
+        buffer: require.resolve("buffer"),
+    }
+    config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"]
+    config.plugins = [
+        ...config.plugins,
         new webpack.ProvidePlugin({
             process: "process/browser",
             Buffer: ["buffer", "Buffer"],
         }),
-    ]);
-    return config;
-};
+    ]
+    // console.log(config.resolve)
+    // console.log(config.plugins)
+
+    return config
+}
