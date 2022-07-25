f9a207e96a5fedfa062c082e721af0a05e45cf31
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-25 13:06:53 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,9 +9,9 @@ import Main from '../components/Main'
 function Home () {
 
     return (
-        <div className="scroll-smooth bg-black">
+        <div className="scroll-smooth bg-black" onTouchStart={closeMenu}>
             <Header />
-            <div id="first_div" className="w-full h-screen bg-blue-600 transition-colors duration-700"  onTouchStart={closeMenu}>
+            <div id="first_div" className="w-full h-screen bg-blue-600 transition-colors duration-700">
                 <div className="flex flex-col justify-center max-w-xs mx-auto w-full h-5/6">
                     <button id="sign_in_btn" className='max-w-xs mx-auto bg-white shadow-2xl rounded-full w-48 md:w-64 h-10 md:h-12 text-sm md:text-base items-center text-blue-500 font-bold select-none hover:scale-110 ease-in-out duration-300'>
                         Connect
@@ -19,10 +19,10 @@ function Home () {
                 </div>
                 <Arrow />
             </div>
-            <div id="second_div" className="w-full bg-blue-600 transition-colors duration-700" onTouchStart={closeMenu}>
+            <div id="second_div" className="w-full bg-blue-600 transition-colors duration-700">
                 <Main />
                 <Team />
-                <div className="w-full h-screen" onClick={closeMenu}>
+                <div className="w-full h-screen">
 
                 </div>
             </div>
