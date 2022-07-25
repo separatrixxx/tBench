674d24882812ae53164853c18f6bbc456c78d391
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-07-25 12:41:58 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ import ScrollIntoView from "react-scroll-into-view";
 function Footer() {
     return(
         <div className="flex flex-col items-center w-full bg-black mt-10">
-            <div className="flex flex-col md:flex-row md:flex-row justify-between items-center w-full md:w-2/3 pb-0 md:pb-10 pt-10 px-10 border-t">
+            <div className="flex flex-col lg:flex-row lg:flex-row justify-between items-center w-full lg:w-2/3 pb-0 lg:pb-10 pt-10 px-10 border-t">
                 <div className="flex justify-center">
                     <ScrollIntoView selector="#first_div">
-                        <h1 className="cursor-pointer mb-10 md:mb-0">
+                        <h1 className="cursor-pointer mb-10 lg:mb-0">
                             <img className="w-6 select-none" draggable="false" src={logo} alt="logo_icon" />
                         </h1>
                     </ScrollIntoView>
-                    <p className="text-white text-sm font-medium ml-5 hidden md:block">© 2022 <span className="font-semibold">tBench</span></p>
+                    <p className="text-white text-sm font-medium ml-5 hidden lg:block">© 2022 <span className="font-semibold">tBench</span></p>
                 </div>
                 <div className="flex justify-center">
                     <a id="vk" className="text-2xl text-white mx-5 transition-colors duration-300" href="/">
@@ -33,7 +33,7 @@ function Footer() {
                         <FaTwitter/>
                     </a>
                 </div>
-                <p className="text-white text-sm font-medium mt-12 block md:hidden">© 2022 <span className="font-semibold">tBench</span></p>
+                <p className="text-white text-sm font-medium mt-12 block lg:hidden">© 2022 <span className="font-semibold">tBench</span></p>
             </div>
             <p className="text-white text-xs m-5">by <span className="font-bold">@separatrix</span></p>
         </div>
