3bff74340041de8bb160e13541a871e7fa49025a
Separatrix <87654656+Separatrixxx@users.noreply.github.com>
2022-08-08 12:29:37 +0300

    Update Arrow.js

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -6,7 +6,7 @@ function Arrow() {
     return(
         <div className="flex align-center justify-center mb-5 h-1/6">
             <ScrollIntoView selector="#second_div">
-                <h1 className="text-4xl sm:m-2 sm:p-2 text-white hover:bg-white rounded-full hover:text-blue-500 hover:drop-shadow-xl transition-colors duration-300 animate-bounce">
+                <h1 className="text-4xl sm:m-2 sm:p-2 text-white hover:bg-white rounded-full hover:text-blue-500 hover:drop-shadow-xl transition-colors duration-300 animate-bounce cursor-pointer">
                     <IoIosArrowDown/>
                 </h1>
             </ScrollIntoView>
