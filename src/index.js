534f6a3b4fe24b713b9fb10d794e7f893b585eb6
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-08-14 22:53:12 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,188 +2,73 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as nearAPI from "near-api-js";
-import getConfig from './config.js';
 
-async function initContract() {
-
-    const nearConfig = getConfig(process.env.NEAR_ENV || 'testnet');
-
-    const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
-
-    const near = await nearAPI.connect({ keyStore, ...nearConfig });
-
-    const walletConnection = new nearAPI.WalletConnection(near);
-
-    let currentUser;
-    if (walletConnection.getAccountId()) {
-        currentUser = {
-            // Gets the accountId as a string
-            accountId: walletConnection.getAccountId(),
-            // Gets the user's token balance
-            balance: (await walletConnection.account().state()).amount,
-        };
-    }
-
-    const contract = await new nearAPI.Contract(
-        walletConnection.account(),
-        nearConfig.contractName,
-        {
-            viewMethods: ['getMessages'],
-            changeMethods: ['addMessage'],
-            sender: walletConnection.getAccountId(),
-        }
-    );
-
-    return { contract, currentUser, nearConfig, walletConnection };
-}
-
-window.nearInitPromise = initContract().then(
-    ({ contract, currentUser, nearConfig, walletConnection }) => {
-        ReactDOM.render(
-            <App
-                contract={contract}
-                currentUser={currentUser}
-                nearConfig={nearConfig}
-                wallet={walletConnection}
-            />,
-            document.getElementById('root')
-        );
-
-        if (walletConnection.isSignedIn()) {
-            isSignedIn(walletConnection, currentUser)
-        } else {
-            isSignedOut()
-        }
-
-        document.querySelector('#sign_in_btn')?.addEventListener('click', () => {
-            walletConnection.requestSignIn(
-                {contractId: nearConfig.contractName, methodNames: [contract.addMessage.name]}, //contract requesting access
-                'tBench', //optional name
-                null, //optional URL to redirect to if the sign in was successful
-                null //optional URL to redirect to if the sign in was NOT successful
-            );
-        })
-
-        document.querySelector('#sign_out_btn')?.addEventListener('click', () => {
-            window.location.href = '/';
-            walletConnection.signOut();
-        })
-
-        document.querySelector('#back_btn')?.addEventListener('touchstart', handleTouchStart, false);
-        document.querySelector('#back_btn')?.addEventListener('touchmove', handleTouchMove, false);
-
-        let xDown = null;
-        let yDown = null;
-
-        function getTouches(evt) {
-            return evt.touches || // чистый API JS
-                evt.originalEvent.touches; // jQuery
-        }
-
-        function handleTouchStart(evt) {
-            const firstTouch = getTouches(evt)[0];
-            xDown = firstTouch.clientX;
-            yDown = firstTouch.clientY;
-        }
-
-        function handleTouchMove(evt) {
-            if ( ! xDown || ! yDown ) {
-                return;
-            }
-
-            const xUp = evt.touches[0].clientX;
-            const yUp = evt.touches[0].clientY;
-
-            const xDiff = xDown - xUp;
-            const yDiff = yDown - yUp;
-
-            if (!(Math.abs(xDiff) <= Math.abs(yDiff))) {/* отлавливаем разницу в движении */
-                if (!(xDiff >= 0)) {
-                    window.location.href = '/';
-                }
-            }
-            /* свайп был, обнуляем координаты */
-            xDown = null;
-            yDown = null;
-        }
-
-
-    }
-
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-function isSignedIn(walletConnection, currentUser) {
 
-    let user = {
-        name: walletConnection.getAccountId().replace(new RegExp('.testnet', 'g'), ''),
-        info: `Hi there, I'm ${walletConnection.getAccountId().replace(new RegExp('.testnet', 'g'), '')}`
-    };
+let loginTitle = document.getElementById('login_title');
+let registrationTitle = document.getElementById('registration_title');
+let forgotPasswordTitle = document.getElementById('forgot_password_title');
 
-    if (document.querySelector('#username') !== null) {
-        document.querySelector('#username').innerHTML = user.name;
-        document.querySelector('#balance').innerHTML = (currentUser.balance / 10**24).toFixed(5) + ' NEAR';
-        document.querySelector('#userInfo').innerHTML = user.info;
-    }
+let loginForm = document.getElementById('login_form');
+let registrationForm = document.getElementById('registration_form');
+let forgotPasswordForm = document.getElementById('forgot_password_form');
 
-    let lastScroll = 0;
-    const defaultOffset = 200;
-    const header = document.querySelector('#content_nav_div');
+loginTitle?.addEventListener('click', () => {
+    registrationTitle?.classList.remove('bg-green-600');
+    registrationTitle?.classList.remove('scale-110');
+    registrationTitle?.classList.remove('text-white');
+    registrationTitle?.classList.add('text-black');
+    registrationTitle?.classList.add('hover:bg-black/10');
 
-    const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+    loginTitle?.classList.add('bg-green-600');
+    loginTitle?.classList.add('scale-110');
+    loginTitle?.classList.add('text-white');
+    loginTitle?.classList.remove('text-black');
+    loginTitle?.classList.remove('hover:bg-black/10');
 
-    const containHide = () => header?.classList.contains('hide');
+    loginForm?.classList.remove('hidden');
+    registrationForm?.classList.add('hidden');
+    forgotPasswordForm?.classList.add('hidden');
+})
 
-    window.addEventListener('scroll', () => {
-        if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-            header?.classList.add('hide');
-        }
-        else if(scrollPosition() < lastScroll && containHide()){
-            header?.classList.remove('hide');
-        }
+registrationTitle?.addEventListener('click', () => {
+    registrationTitle?.classList.add('bg-green-600');
+    registrationTitle?.classList.add('scale-110');
+    registrationTitle?.classList.add('text-white');
+    registrationTitle?.classList.remove('text-black');
+    registrationTitle?.classList.remove('hover:bg-black/10');
 
-        lastScroll = scrollPosition();
-    })
+    loginTitle?.classList.remove('bg-green-600');
+    loginTitle?.classList.remove('scale-110');
+    loginTitle?.classList.remove('text-white');
+    loginTitle?.classList.add('text-black');
+    loginTitle?.classList.add('hover:bg-black/10');
 
-}
+    loginForm?.classList.add('hidden');
+    registrationForm?.classList.remove('hidden');
+    forgotPasswordForm?.classList.add('hidden');
+})
 
-function isSignedOut() {
+forgotPasswordTitle?.addEventListener('click', () => {
+    registrationTitle?.classList.remove('bg-green-600');
+    registrationTitle?.classList.remove('scale-110');
+    registrationTitle?.classList.remove('text-white');
+    registrationTitle?.classList.add('text-black');
+    registrationTitle?.classList.add('hover:bg-black/10');
 
-    document.querySelector('.link')?.addEventListener('click', () => {
-        window.location.reload();
-    })
+    loginTitle?.classList.remove('bg-green-600');
+    loginTitle?.classList.remove('scale-110');
+    loginTitle?.classList.remove('text-white');
+    loginTitle?.classList.add('text-black');
+    loginTitle?.classList.add('hover:bg-black/10');
 
-    let lastScroll = 0;
-    const defaultOffset = 200;
-    const header = document.querySelector('#nav_div');
-
-    const height = window.innerHeight
-    const div1 = document.querySelector('#first_div');
-    const div2 = document.querySelector('#second_div');
-
-    const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-    const containHide = () => header.classList.contains('hide');
-
-    window.addEventListener('scroll', () => {
-        if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-            header.classList.add('hide');
-        }
-        else if(scrollPosition() < lastScroll && containHide()){
-            header.classList.remove('hide');
-        }
-
-        lastScroll = scrollPosition();
-
-        if (scrollPosition() > height - 1) {
-            div1.classList.add("bg-black");
-            div1.classList.remove("bg-blue-600");
-            div2.classList.add("bg-black");
-            div2.classList.remove("bg-blue-600");
-        } else {
-            div1.classList.remove("bg-black");
-            div1.classList.add("bg-blue-600");
-            div2.classList.remove("bg-black");
-            div2.classList.add("bg-blue-600");
-        }
-    })
-}
+    loginForm?.classList.add('hidden');
+    registrationForm?.classList.add('hidden');
+    forgotPasswordForm?.classList.remove('hidden');
+})
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Emoji, EmojiProvider} from "react-apple-emojis";
-import emojiData from 'react-apple-emojis/src/data.json';
-
 
 function NotFound () {
 
     return (
-        <div className="scroll-smooth bg-blue-600 w-full h-screen flex flex-col justify-center items-center">
-            <a href="/">
-                <EmojiProvider data={emojiData} ><Emoji name="pensive-face" width={120} /></EmojiProvider>
-            </a>
-            <a href="/" className="mt-10 md:mt-16 text-xl md:text-5xl text-white hover:text-gray-300 transition-colors duration-500 font-black">Ooops... Looks like page not found :(</a>
+        <div className="scroll-smooth bg-green-600 w-full h-screen flex flex-col justify-center items-center p-5">
+            <a href="/" className="mt-10 md:mt-16 text-xl md:text-5xl text-white text-center hover:text-gray-300 transition-colors duration-500 font-black">На этой странице виноград точно не растёт</a>
         </div>
     );
 }
