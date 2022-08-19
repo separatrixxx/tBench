cfc0f2c8586849393c7bbe54e2cea8d1c3adadb6
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-08-19 17:13:40 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '144': '36rem',
+      },
+      colors: {
+        'blue-tb': '#0080ff',
+      },
+    },
   },
   variants: {
     extend: {},
