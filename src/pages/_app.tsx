dbc0237d3e279517db9dceef47d9b6381091903f
separatrixxx <nikitalochmatov@gmail.com>
2023-01-16 13:37:15 +0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return <>
     <Head>
       <title>tBench</title>
-      <meta name='description' content='Онлайн-кинотеатр, на котором собрано множество фильмов и сериалов, доступных
-      для просмотра бесплатно.' />
-      <meta property='og:title' content='[xd] - Онлайн-кинотеатр' />
-      <meta property='og:description' content='Онлайн-кинотеатр, на котором собрано множество фильмов и сериалов, доступных
-      для просмотра бесплатно.' />
+      <meta name='description' content='Децентрализованная социальная сеть tBench.' />
+      <meta property='og:title' content='tBecnh' />
+      <meta property='og:description' content='Децентрализованная социальная сеть tBench.' />
       <meta property='og:locale' content='ru_RU' />
       <link rel="icon" href="/logo_icon_circle.svg" />
     </Head>
