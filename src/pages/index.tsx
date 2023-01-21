eb2f28e7b2aaa5a9413298c4be5fc385d0d0ec7d
separatrixxx <nikitalochmatov@gmail.com>
2023-01-21 15:21:02 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { en } from "locales/en.locale";
 import { ru } from "locales/ru.locale";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
+  console.log(router)
   const t = router.locale === 'en' ? en : ru;
   return (
     <>
+      <Link href={router.asPath} locale="ru"><p>ru</p></Link>
+      <Link href={router.asPath} locale="en"><p>en</p></Link>
       {t.title + 'tBench'}
     </>
   );
