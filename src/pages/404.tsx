33becfc223a23bb7fdce92042190c2d63af2ec34
separatrixxx <nikitalochmatov@gmail.com>
2023-01-21 16:17:08 +0300

    update

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { ErrorPage } from 'page_components/ErrorPage/ErrorPage';
+
+export const PageNotFound = () => {
+	return (
+		<>
+			<Head>
+				<title>{'tBench - 404'}</title>
+			</Head>
+            <ErrorPage error={404} />
+		</>
+	);
+}
+
+export default PageNotFound;
