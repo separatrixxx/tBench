6c9fa1b74e0887126a21808ca77f3854c10844f6
separatrixxx <nikitalochmatov@gmail.com>
2023-01-21 20:33:19 +0300

    update

---
+++ page_components/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import styles from './HomePage.module.css';
+
+export const HomePage = (): JSX.Element => {
+	return (
+        <div className={styles.wrapper}>
+            <div className={styles.block1}>
+
+            </div>
+            <div className={styles.block2}>
+
+            </div>
+        </div>
+    );
+};
