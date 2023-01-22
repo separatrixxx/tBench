f9a2cbefcc6b279bf5061708811147f728e3f7b7
separatrixxx <nikitalochmatov@gmail.com>
2023-01-22 13:58:00 +0300

    update

--- page_components/HomePage/HomePage.tsx
+++ page_components/HomePage/HomePage.tsx
@@ -1,15 +1,20 @@
 import styles from './HomePage.module.css';
 import { Header } from "components/Header/Header";
 import { AuthForm } from 'components/AuthForm/AuthForm';
+import Image from 'next/image'
 
 export const HomePage = (): JSX.Element => {
 	return (
         <>
             <Header />
             <div className={styles.wrapper}>
-                <div className={styles.startBlock}>
-
-                </div>
+                <Image className={styles.startBlock}
+					loader={() => '/home_image.jpeg'}
+					src='/home_image.jpeg'
+					alt='home image'
+					width={1}
+					height={1}
+				/>
                 <AuthForm />
             </div>
         </>
