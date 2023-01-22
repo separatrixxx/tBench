7fb2d05d54c6d88b70eb2fedc5df60a0c6d2bcef
separatrixxx <nikitalochmatov@gmail.com>
2023-01-22 20:48:10 +0300

    update

--- page_components/ErrorPage/ErrorPage.tsx
+++ page_components/ErrorPage/ErrorPage.tsx
@@ -1,15 +1,18 @@
 import { setLocale } from "helpers/helpers_locale";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { ErrorPageProps } from "./ErrorPage.props";
 
 export const ErrorPage = ({ error }: ErrorPageProps): JSX.Element => {
+    const router = useRouter();
+    
 	if (error === 404) {
         return (
-            <Link href='/'><p>{setLocale().error404}</p></Link>
+            <Link href='/'><p>{setLocale(router.locale).error404}</p></Link>
         );
     } else {
         return (
-            <Link href='/'><p>{setLocale().error500}</p></Link>
+            <Link href='/'><p>{setLocale(router.locale).error500}</p></Link>
 	    );
     }
 };
