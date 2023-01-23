4e32a879fd84bfafdcccc7243196ef1e2229503b
separatrixxx <nikitalochmatov@gmail.com>
2023-01-23 11:03:41 +0300

    update

--- locales/ru.locale.ts
+++ locales/ru.locale.ts
@@ -2,7 +2,7 @@ export const ru = {
     title: "Добро пожаловать на ",
     error404: "Страница не найдена, ошибка 404",
     error500: "Нарушение работы сервера, ошибка 500",
-    email: "E-mail",
+    email: "Электронная почта",
     password: "Пароль",
     confirm_password: "Повторите пароль",
     first_name: "Имя",
