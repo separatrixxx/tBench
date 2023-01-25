e70bf72d5a7efb4cb68150957cf5963dcd7c2e23
separatrixxx <nikitalochmatov@gmail.com>
2023-01-25 21:05:33 +0300

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
 import styles from './Header.module.css';
+import Logo from './logo_icon.svg';
 
 export const Header = (): JSX.Element => {
     return (
         <header className={styles.header}>
+            <Link href='/' className={styles.logo}><Logo /></Link>
         </header>
     );
 };
