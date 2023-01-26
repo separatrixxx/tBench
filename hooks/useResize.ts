9b32fb81b00dfd8e0eed8b89ca58bf5d68b1b028
separatrixxx <nikitalochmatov@gmail.com>
2023-01-26 23:29:20 +0300

    update

--- hooks/useResize.ts
+++ hooks/useResize.ts
@@ -4,6 +4,8 @@ export const useResize = (): number => {
 	const [resize, setResize] = useState<number>(0);
 
 	useEffect(() => {
+		setResize(window.innerWidth);
+		
 		window.addEventListener('resize', function(){
             setResize(window.innerWidth);
         });
