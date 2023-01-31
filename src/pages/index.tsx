59ad80a27ea1fa144dcb1c1c019ab4f480e093cf
separatrixxx <nikitalochmatov@gmail.com>
2023-01-31 19:58:18 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
+import { useRouter } from "next/router";
 import { HomePage } from "page_components/HomePage/HomePage";
+import { useEffect } from "react";
 
 function Home(): JSX.Element {
+  const router = useRouter();
+  
+  useEffect(() => {
+    const loggedIn = localStorage.getItem('logged_in');
+
+    if (loggedIn) {
+      router.push('/content');
+    }
+  });
+
   return (
     <HomePage />
   );
