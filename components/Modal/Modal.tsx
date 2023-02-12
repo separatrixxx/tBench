f2d04ef09f5f2f6324656c7cb3863cc1417e5583
Nikita <nikitalochmatov@gmail.com>
2023-02-12 18:16:56 +0300

    update

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -5,31 +5,31 @@ import { motion } from 'framer-motion';
 
 export const Modal = ({ active, setActive, children }: ModalProps): JSX.Element => {
     const variants = {
-		visible: {
-			opacity: 1,
-		},
-		hidden: { 
+        visible: {
+            opacity: 1,
+        },
+        hidden: {
             opacity: 0,
         }
-	};
+    };
 
     const variantsModal = {
-		visible: {
-			scale: 1,
-		},
-		hidden: { 
-            scale: 0,
+        visible: {
+            scale: 1,
+        },
+        hidden: {
+            scale: 0.5,
         }
-	};
+    };
 
-	return (
+    return (
         <motion.div className={cn(styles.modal, {
             [styles.active]: active,
         })} onClick={() => setActive(false)}
             variants={variants}
             initial={active ? 'visible' : 'hidden'}
             transition={{ duration: 0.15 }}
-			animate={active ? 'visible' : 'hidden'}>
+            animate={active ? 'visible' : 'hidden'}>
             <motion.div className={styles.modalContent} onClick={e => e.stopPropagation()}
                 variants={variantsModal}
                 initial={active ? 'visible' : 'hidden'}
