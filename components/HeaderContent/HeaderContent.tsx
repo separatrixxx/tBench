842c58de9c8441e6ec77e973f3afdc83efbef692
Nikita <nikitalochmatov@gmail.com>
2023-02-17 20:46:03 +0300

    update

--- components/HeaderContent/HeaderContent.tsx
+++ components/HeaderContent/HeaderContent.tsx
@@ -1,11 +1,18 @@
+import styles from './HeaderContent.module.css';
 import { HeaderUserIcon } from 'components/HeaderUserIcon/HeaderUserIcon';
 import { InputContent } from 'components/InputContent/InputContent';
-import styles from './HeaderContent.module.css';
+import { useContext } from 'react';
+import { AppContext } from 'context/app.context';
+import cn from 'classnames';
 
 
-export const HeaderContent = (): JSX.Element => {    
+export const HeaderContent = (): JSX.Element => {
+    const context = useContext(AppContext);
+
     return (
-        <header className={styles.headerContent}>
+        <header className={cn(styles.headerContent, {
+            [styles.darkThemeHeaderContent]: context.theme === 'dark',
+        })}>
             <InputContent />
             <HeaderUserIcon />
         </header>
