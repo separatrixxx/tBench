149550c736c4f281e1072e3a14861f209c3433f7
separatrixxx <nikitalochmatov@gmail.com>
2023-02-25 16:40:01 +0300

    update

--- components/ChangeTheme/ChangeTheme.tsx
+++ components/ChangeTheme/ChangeTheme.tsx
@@ -4,13 +4,27 @@ import { BsSun, BsMoon } from "react-icons/bs";
 import { useContext } from 'react';
 import { AppContext } from 'context/app.context';
 import cn from 'classnames';
+import { useScrollY } from 'hooks/useScrollY';
+import { useResizeW } from 'hooks/useResize';
 
 export const ChangeTheme = ({ setTheme }: ChangeThemeProps): JSX.Element => {
     const context = useContext(AppContext);
 
+    const scrollPosition = useScrollY();
+    const width = useResizeW();
+
+    let opacity = 1;
+
+    if (width < 1024) {
+        opacity = opacity - scrollPosition / 100;
+    } else {
+        opacity = opacity - scrollPosition / 150;
+        console.log(scrollPosition)
+    }
+
     if (context.theme === 'light') {
         return (
-            <span className={styles.changeTheme} onClick={() => {
+            <span className={styles.changeTheme} style={{opacity: opacity}} onClick={() => {
                 context.setTheme?.('dark');
                 setTheme?.('dark');
                 localStorage.setItem('theme', 'dark');
@@ -20,7 +34,7 @@ export const ChangeTheme = ({ setTheme }: ChangeThemeProps): JSX.Element => {
         );
     } else {
         return (
-            <span className={cn(styles.changeTheme, styles.darkChangeTheme)} onClick={() => {
+            <span className={cn(styles.changeTheme, styles.darkChangeTheme)} style={{opacity: opacity}} onClick={() => {
                 context.setTheme?.('light');
                 setTheme?.('light');
                 localStorage.setItem('theme', 'light');
