3b08f87c0bcb900c3818c06dabbd1374ba142fe6
Nikita <nikitalochmatov@gmail.com>
2023-03-27 22:23:09 +0300

    update

--- components/ProfileCover/ProfileCover.tsx
+++ components/ProfileCover/ProfileCover.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image';
 
 
 export const ProfileCover = ({ active, setActive, setType }: ProfileCoverProps): JSX.Element => {
-    const cover = '/covr.jpg';
+    const cover = '/cover.jpg';
 
     return (
         <Image className={styles.profileCover} draggable='false'
             loader={() => cover}
             src={cover}
             alt='image'
-            width={1}
-            height={1}
+            width={1000}
+            height={1000}
             unoptimized={true}
             priority={true}
             onClick={() => {
