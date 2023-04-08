ada04dee19819afbedcf80d7f4add7d821b4abf4
Nikita <nikitalochmatov@gmail.com>
2023-04-09 01:49:11 +0300

    update

--- client/wow/Looking3D/Looking3D.tsx
+++ client/wow/Looking3D/Looking3D.tsx
@@ -1,29 +1,27 @@
-import React, { Suspense } from 'react';
 import styles from './Looking3D.module.css';
-import { OrbitControls } from "@react-three/drei";
+import React, { Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
 
 
 const Model = React.lazy(() => import('./Model'));
 
+
 export const Looking3D = (): JSX.Element => {
 	return (
-		<div className={styles.lookingWrapper}>
-			{/* <Canvas className="z-10 mx-10"
-				camera={{ position: [1, 0, 10], fov: 15 }}
+		<div id='wrapper' className={styles.lookingWrapper}>
+			<Canvas className="z-10 mx-10"
+				camera={{ position: [1, 0, 100], fov: 15 }}
 				style={{
-					width: '96vw',
-					height: '100vh'
+					width: '80vw',
+					height: '80vh',
 				}}
 			>
-				<ambientLight intensity={1.25} />
 				<ambientLight intensity={0.1} />
 				<directionalLight intensity={0.4} />
 				<Suspense fallback={null}>
 					<Model position={[0, 0, 0]} />
 				</Suspense>
-				<OrbitControls />
-			</Canvas> */}
+			</Canvas>
 		</div>
 	);
 };
