a7f350998586ca5761ceb3cace490ca4179d9451
Nikita <nikitalochmatov@gmail.com>
2023-05-06 01:22:24 +0300

    update

--- client/page_components/ContentPage/ContentPage.tsx
+++ client/page_components/ContentPage/ContentPage.tsx
@@ -5,17 +5,23 @@ import { AppContextProvider } from 'context/app.context';
 import { InputContent } from 'components/InputContent/InputContent';
 import { HeaderUserIcon } from 'components/HeaderUserIcon/HeaderUserIcon';
 import { ContentList } from 'components/ContentList/ContentList';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { ContentModal } from 'components/ContentModal/ContentModal';
-import cn from 'classnames';
 import { NotificationsButton } from 'components/NotificationsButton/NotificationsButton';
+import { Htag } from 'components/Htag/Htag';
+import { setLocale } from 'helpers/locale.helper';
+import cn from 'classnames';
 
 
 export const ContentPage = ({ theme }: ContentPageProps): JSX.Element => {
+    const router = useRouter();
+
     const [activeContent, setActiveContent] = useState<boolean>(false);
     const [image, setImage] = useState<string>('');
     const [postId, setPostId] = useState<number>(NaN);
     const [typeContent, setTypeContent] = useState<'post' | 'comments'>('post');
+    const [page, setPage] = useState<'content' | 'messages'>('content');
 
     return (
         <AppContextProvider theme={theme} >
@@ -27,6 +33,18 @@ export const ContentPage = ({ theme }: ContentPageProps): JSX.Element => {
                     <NotificationsButton isNotification={true} />
                     <HeaderUserIcon className={styles.contentUserIcon} user='profile' userImage='/rainbow.jpg' />
                 </HeaderContent>
+                <div className={styles.pagesSwitch}>
+                    <Htag tag='xl' className={cn(styles.switchText, {
+                        [styles.darkThemeSwitchText]: theme === 'dark',
+                        [styles.dontActiveText]: page !== 'content' && theme !== 'dark',
+                        [styles.darkThemeDontActiveText]: page !== 'content' && theme === 'dark',
+                    })} onClick={() => setPage('content')}>{setLocale(router.locale).content}</Htag>
+                    <Htag tag='xl' className={cn(styles.switchText, {
+                        [styles.darkThemeSwitchText]: theme === 'dark',
+                        [styles.dontActiveText]: page !== 'messages' && theme !== 'dark',
+                        [styles.darkThemeDontActiveText]: page !== 'messages' && theme === 'dark',
+                    })} onClick={() => setPage('messages')}>{setLocale(router.locale).messages}</Htag>
+                </div>
                 <ContentList setType={setTypeContent} setActive={setActiveContent} setImage={setImage} setPostId={setPostId} />
                 <ContentModal type={typeContent} active={activeContent} setActive={setActiveContent} image={image} postId={postId} />
             </div>
