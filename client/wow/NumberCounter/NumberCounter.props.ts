71c6553f0c0498918fdb78c67c1fc7a70a53a3d4
Nikita <nikitalochmatov@gmail.com>
2023-07-18 01:12:52 +0300

    update

--- client/wow/NumberCounter/NumberCounter.props.ts
+++ client/wow/NumberCounter/NumberCounter.props.ts
@@ -1,6 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 export interface NumberCounterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	id: string,
 	number: number,
 	time: number,
 	step: number,
--- client/wow/WowPage/WowPage.tsx
+++ client/wow/WowPage/WowPage.tsx
@@ -31,7 +31,11 @@ export const WowPage = (): JSX.Element => {
 			<Htag tag='l' className={styles.text}>Wow Cursor</Htag>
 			<WowCursor />
 			<Htag tag='l' className={styles.text}>Number Counter</Htag>
-			<NumberCounter number={1603} time={700} step={1} />
+			<Htag tag='l' className={styles.text}>Number Counter</Htag>
+			<Htag tag='l' className={styles.text}>Number Counter</Htag>
+			<Htag tag='l' className={styles.text}>Number Counter</Htag>
+			<Htag tag='l' className={styles.text}>Number Counter</Htag>
+			<NumberCounter id='c1' number={1603} time={7000} step={1} />
 			<Htag tag='l' className={styles.text}>Wow Button</Htag>
 			<WowButton />
 			{/* <Htag tag='l' className={styles.text}>Looking 3D</Htag>
