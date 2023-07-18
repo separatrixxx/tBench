415e2e3b5418c96dcbc0b5a41345a590b135800e
biggvladik <zuevvlad55@gmail.com>
2023-07-18 13:44:50 +0300

    docker - compose

--- server/authentication service/database.py
+++ server/authentication service/database.py
@@ -4,7 +4,7 @@ from bson.objectid import ObjectId
 import traceback
 class Database():
     def __init__(self):
-        self.client = motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+        self.client = motor_asyncio.AsyncIOMotorClient("mongodb://mongodb:27017/")
         self.database = self.client.users
         self.user_collection = self.database.get_collection('users_collection')
 
