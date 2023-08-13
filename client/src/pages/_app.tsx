aa496d4d0737af8a2aff724a0cb2e54a0ab2449f
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2023-08-13 21:11:55 +0300

    redux update (#29)
    * redux update
    
    * update
    
    * redux update
    
    * redux update

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,22 +3,27 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import { wrapper } from '../../features/store/store';
+import { Provider } from 'react-redux';
 
 
 function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Head>
-      <title>tBench</title>
-      <meta name='description' content='Decentralized social network tBench. A unique place to start
-        and develop your brands, ideas and creativity.' />
-      <meta property='og:title' content='tBench' />
-      <meta property='og:description' content='Decentralized social network tBench. A unique place to start
-        and develop your brands, ideas and creativity.' />
-      <meta charSet="utf-8" />
-      <link rel="icon" href="/logo_icon_circle.svg" type='image/svg+xml' />
-    </Head>
-    <Component {...pageProps} />
-  </>;
+  const { store } = wrapper.useWrappedStore(pageProps);
+
+  return (
+    <Provider store={store}>
+      <Head>
+        <title>tBench</title>
+        <meta name='description' content='Decentralized social network tBench. A unique place to start
+          and develop your brands, ideas and creativity.' />
+        <meta property='og:title' content='tBench' />
+        <meta property='og:description' content='Decentralized social network tBench. A unique place to start
+          and develop your brands, ideas and creativity.' />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/logo_icon_circle.svg" type='image/svg+xml' />
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default wrapper.withRedux(App);
+export default App;
