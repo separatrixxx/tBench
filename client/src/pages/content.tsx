c28da018a95b0a887fb027b30ae8b1c139c22c90
separatrixxx <nikitalochmatov@gmail.com>
2023-08-13 21:21:54 +0300

    redux update

--- client/src/pages/content.tsx
+++ client/src/pages/content.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 import { setLocale } from 'helpers/locale.helper';
 import { pageHelper, userHelper } from 'helpers/pages.helper';
-import { User } from "interfaces/user.interface";
 import { useDispatch } from "react-redux";
 
 
